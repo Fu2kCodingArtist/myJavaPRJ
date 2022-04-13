c27d5b19f9d9516afd579b10707f191658ca1871
Fu2kCodingArtist <2220110025@office.kopo.ac.kr>
2022-04-13 11:24:55 +0900

    2022_04_13

---
+++ src/main/java/Study_01.java
@@ -0,0 +1,10 @@
+public class Study_01 {
+    public static void main(String[] args) {
+        int i;
+        i = 0;
+
+        while ( i < 5 ) {
+            System.out.printf("while 반복문.\n");
+        }
+    }
+}
